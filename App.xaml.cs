2dcd558eaf23837b4b96dd036ddb14ec960aa375
ForzSaex <creepergamerbr46@gmail.com>
2024-09-05 11:25:41 -0300

    Horizon Base

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Horizon
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
